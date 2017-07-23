79835e677e65faa4256ba563a7ae19c7ca92ed09
vinay manyam <vinayip541@gmail.com>
2017-07-23 14:39:36 +0530

    Add files via upload

---
+++ head/select.php
@@ -0,0 +1,4 @@
+  <select id="pro" style="background-image:url(user.png);" onchange="">
+                                                <option name="uname" >Hello <?php echo $_SESSION['name']?></options>
+                                                    <option id="logout">Logout</options>
+  </select>
---
+++ head/select2.php
@@ -0,0 +1,8 @@
+  <select id="pro" style="background-image:url(user.png);" onchange="">
+                                                <option name="uname" >Hello <?php echo $_SESSION['name']?></options>
+                                                <option id="account" value="account">My Account</options>
+                                                <option id="account" value="add">Add New</options>
+                                                <option id="account" value="all">My Products</options>
+                                                <option id="logout" value="logout">Logout</options>
+                                                    
+  </select>
