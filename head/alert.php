782ee9b48a198209927b21cc86f2dfd39b591fac
vinay manyam <vinayip541@gmail.com>
2017-07-23 14:39:17 +0530

    Create alert.php

---
+++ head/alert.php
@@ -0,0 +1,18 @@
+		<div class="top-header navbar navbar-default"><!--header-one-->
+			<div class="container">
+				<div class="nav navbar-nav wow fadeInLeft animated" data-wow-delay=".5s">
+					<p>Welcome to Modern Shoppe <a href="register.php">Register </a> Or <a href="signin.php">Sign In </a></p>
+				</div>
+				<div class="nav navbar-nav navbar-right social-icons wow fadeInRight animated" data-wow-delay=".5s">
+					<ul>
+						<li><a href="#"> </a></li>
+						<li><a href="#" class="pin"> </a></li>
+						<li><a href="#" class="in"> </a></li>
+						<li><a href="#" class="be"> </a></li>
+						<li><a href="#" class="you"> </a></li>
+						<li><a href="#" class="vimeo"> </a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
